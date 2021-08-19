e18d1cb19d0f9e80c3825e6a4ffb1d728dc2173d
arthur.zavadski <aczavads@gmail.com>
2021-08-19 01:06:22 +0000

    ProdutoController com delete e get by id.

--- src/main/java/br/unicesumar/adsis4s2021/produto/ProdutoController.java
+++ src/main/java/br/unicesumar/adsis4s2021/produto/ProdutoController.java
@@ -3,7 +3,9 @@ package br.unicesumar.adsis4s2021.produto;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repo;
+
 	
+	@DeleteMapping("/{id}")
+	public void deletePeloId(@PathVariable("id") String id) {
+		repo.deleteById(id);
+	}
+	
+	@GetMapping("/{id}")
+	public Produto getPeloId(@PathVariable("id") String id) {
+		return repo.findById(id).get();
+	}
+
 	
 	@GetMapping
-	public List<Produto> getProdutos() {
+	public List<Produto> getAll() {
 		return repo.findAll();
 	}
 	
 	@PostMapping
-	public String postProduto(@RequestBody Produto novo) {
+	public String post(@RequestBody Produto novo) {
 		novo = repo.save(novo);
 		return novo.getId();
 	}	

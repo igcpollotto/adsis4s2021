43170ff41176a06acc5c0ae09ad4921326792b9e
arthur.zavadski <aczavads@gmail.com>
2021-08-20 23:39:32 +0000

    Exemplo com PUT e acesso ao Postgresql.

--- src/main/java/br/unicesumar/adsis4s2021/produto/ProdutoController.java
+++ src/main/java/br/unicesumar/adsis4s2021/produto/ProdutoController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,12 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository repo;
 
+	
+	@GetMapping("/com-preço-maior-que")
+	public List<Produto> getComPreçoMaiorQue(@RequestParam double preço) {
+		//return repo.recuperarPreçoMaiorQue(preço);
+		return repo.recuperarPreçoMaiorQueViaSQL(preço);
+	}
 	
 	@DeleteMapping("/{id}")
 	public void deletePeloId(@PathVariable("id") String id) {
@@ -36,7 +44,23 @@ public class ProdutoController {
 	
 	@PostMapping
 	public String post(@RequestBody Produto novo) {
+		if (repo.findById(novo.getId()).isPresent()) {
+			throw new RuntimeException("Seu produto já existe, faça um put ao invés de post!");
+		}
 		novo = repo.save(novo);
 		return novo.getId();
+	}
+	
+	@PutMapping("/{id}")
+	public String put(@RequestBody Produto modificado, @PathVariable("id") String id) {
+		if (!modificado.getId().equals(id)) {
+			throw new RuntimeException("Para atualizar um produto, os IDs do request devem ser iguais!");
+		}
+		if (!repo.findById(id).isPresent()) {
+			throw new RuntimeException("Seu produto não existe, faça um post ao invés de put!");
+		}
+		modificado = repo.save(modificado);
+		return modificado.getId();
 	}	
+
 }
